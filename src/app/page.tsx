2a275686e63e89e1a2ace5e12f0b3d22846739ea
MuhammadZohaibofficial <muhammadzohaib786king@gmail.com>
2026-02-01 12:13:36 +0500

    Add download link for audio file in Page component
    Added a download link for the audio file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import ClientDynamicTTS from "@/components/ClientDynamicTTS";
 
 export default function Page() {
-  return <ClientDynamicTTS />;
+  const audioUrl = "/my-voice.mp3";
+
+  return (
+    <>
+      <ClientDynamicTTS />
+
+      <a href={audioUrl} download="my-voice.mp3" className="download-btn">
+        Download MP3
+      </a>
+    </>
+  );
 }
